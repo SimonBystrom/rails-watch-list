7c6185157d015f212c468546a59b1c5a7031d9fe
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-20 19:12:20 +0900

    Added hover effect with number of movies in list to list item in index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :img_url)
   end
 end
---
+++ db/migrate/20210520093957_add_image_to_list.rb
@@ -0,0 +1,5 @@
+class AddImageToList < ActiveRecord::Migration[6.0]
+  def change
+    add_column :lists, :img_url, :string
+  end
+end
