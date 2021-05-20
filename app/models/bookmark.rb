5c92d6f5f227707cd7852132bfc5ac56c70488b7
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-20 11:58:38 +0900

    validates movie and list models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :movie_id, :list_id, uniqueness: true
+  validates :comment, presence: true, length: { minimum: 6 }
 end
