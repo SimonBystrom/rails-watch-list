c55538d7df8d2c2629f9354bb8fcacc8eb5237a8
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-21 11:27:23 +0900

    added a root to roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root '/lists', to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     member do
       resources :bookmarks, only: [:new, :create]
